af66aa648664e627ea27a83fa2dc7255eae4a1e6
Gledson Lima <58941795+gledlima@users.noreply.github.com>
2023-11-16 22:27:55 -0300

    Create 2ªPergunta.sql

---
+++ 2ªPergunta.sql
@@ -0,0 +1,15 @@
+
+
+select 
+    categoria.Categoria as 'Tipo de produto', 
+    sum(fatodetalhes_dadosmodelagem.Valor) as 'Valor em vendas por tipo de produto'  
+from 
+    fatodetalhes_dadosmodelagem
+inner join 
+    produtos on fatodetalhes_dadosmodelagem.ProdutoID = produtos.ProdutoID
+inner join 
+    categoria on categoria.CategoriaID = produtos.CategoriaID
+GROUP BY 
+	  categoria.Categoria
+order by 
+    sum(fatodetalhes_dadosmodelagem.Valor) desc;
