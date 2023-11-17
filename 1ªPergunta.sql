36c53a580b7df42fc82f9428aae38390019310b4
Gledson Lima <58941795+gledlima@users.noreply.github.com>
2023-11-17 12:27:09 -0300

    Update 1ªPergunta.sql

--- 1ªPergunta.sql
+++ 1ªPergunta.sql
@@ -1,13 +1,16 @@
 /* 1ª Pergunta: Valor total das vendas e dos fretes por produto e ordem de venda */
 /* Query extraindo o valor total em vendas por produto */
 
-select 
+select
 	produtos.Produto, 
-	sum(fatodetalhes_dadosmodelagem.Valor) as 'Soma das vendas'
+	sum(fatodetalhes_dadosmodelagem.Valor) as 'Valor total das vendas',
+    	sum(fatocabecalho_dadosmodelagemcsv.ValorFrete) as 'Valor total dos Fretes'
 from 
 	fatodetalhes_dadosmodelagem
 join 
-  produtos on fatodetalhes_dadosmodelagem.ProdutoID = produtos.ProdutoID
+        produtos on fatodetalhes_dadosmodelagem.ProdutoID = produtos.ProdutoID
+join 
+        fatocabecalho_dadosmodelagemcsv on fatocabecalho_dadosmodelagemcsv.CupomID = fatodetalhes_dadosmodelagem.CupomID
 GROUP BY 
 	produtos.Produto
 order BY 
